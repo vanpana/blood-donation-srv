b56bda1956a53d82f58c9b49f00fcad2e197b01f
vanpana <van.panaite@gmail.com>
2018-04-04 19:24:50 +0300

    added transport domain

---
+++ src/main/java/com/cyberschnitzel/Domain/Transport/Requests/MessageRequest.java
@@ -0,0 +1,31 @@
+package com.cyberschnitzel.Domain.Transport.Requests;
+
+public class MessageRequest {
+    private String email;
+    private String password;
+    private String token;
+    private String message;
+
+    public MessageRequest(String email, String password, String token, String message) {
+        this.email = email;
+        this.password = password;
+        this.token = token;
+        this.message = message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
