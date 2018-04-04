7fd5301f2a130e7b2aeb22b341fecab0587f2415
vanpana <van.panaite@gmail.com>
2018-04-04 18:46:41 +0300

    added controller

---
+++ src/main/java/com/cyberschnitzel/Domain/Controller/Controller.java
@@ -0,0 +1,8 @@
+package com.cyberschnitzel.Domain.Controller;
+
+import com.cyberschnitzel.Domain.Entities.Entity;
+import com.cyberschnitzel.Repository.DatabaseRepository;
+
+public class Controller {
+    // Keep stuff static here
+}
