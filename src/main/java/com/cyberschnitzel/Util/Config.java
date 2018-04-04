f86b60f878f18580949671d8bcafd3f76e064657
vanpana <van.panaite@gmail.com>
2018-04-04 13:54:15 +0300

    implemented creator script execution at ONE run

--- src/main/java/com/cyberschnitzel/Util/Config.java
+++ src/main/java/com/cyberschnitzel/Util/Config.java
@@ -17,6 +17,8 @@ public class Config {
     private final static String PROPERTY_DB_NAME = "db.name";
     private final static String PROPERTY_DB_USER = "db.user";
     private final static String PROPERTY_DB_PASSWORD = "db.password";
+    private final static String PROPERTY_DB_CREATION = "db.creation";
+    private final static String PROPERTY_DB_CREATION_FILENAME = "db.creation_sql";
 
     /**
      * Get the default config properties file
@@ -27,7 +29,7 @@ public class Config {
     private static File getPropertiesFile() throws ConfigException {
         try {
             mutex.acquire();
-            if (propertiesFile == null) propertiesFile = getPropertiesFile(PROPERTIES_FILENAME);
+            if (propertiesFile == null) propertiesFile = getResourcesFile(PROPERTIES_FILENAME);
         } catch (InterruptedException ie) {
             System.out.println("Failed to acquire Config mutex: " + ie.getMessage());
         } finally {
@@ -43,7 +45,7 @@ public class Config {
      * @return File representing the properties file
      * @throws ConfigException if resource can't be fetched
      */
-    private static File getPropertiesFile(String filename) throws ConfigException {
+    public static File getResourcesFile(String filename) throws ConfigException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         URL url = classLoader.getResource(filename);
 
@@ -57,7 +59,7 @@ public class Config {
      * @param key - the key to be searched for
      * @return value - String representing the value from that key
      */
-    private static String getValueFromPropertiesKey(String key) {
+    private static String getValueFromPropertyKey(String key) {
         Properties properties = new Properties();
         InputStream input = null;
         String value = null;
@@ -91,24 +93,83 @@ public class Config {
         return value;
     }
 
+    private static boolean writeValueToPropertyKey(String key, String value) {
+        Properties properties = new Properties();
+        InputStream input;
+        OutputStream output = null;
+        String comment = "Key: " + key + ", Value: " + value;
+        boolean result = false;
+
+        try {
+            // Get file from resources folder
+            input = new FileInputStream(getPropertiesFile());
+
+            // Load the properties from the file
+            properties.load(input);
+
+            // Set the value to the property key
+            properties.setProperty(key, value);
+
+            // Safely close the input
+            input.close();
+
+            // Open the output stream
+            output = new FileOutputStream(getPropertiesFile());
+
+            // Store the properties file
+            properties.store(output, comment);
+
+            // Set the result of the operation
+            result = true;
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("File not found:" + fnfe.getMessage());
+        } catch (ConfigException ce) {
+            System.out.println("Config exception:" + ce.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("I/O Exception:" + ioe.getMessage());
+        } finally {
+            // Safely close the output
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    System.out.println("Failed closing properties file: " + e.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+
     // DATABASE CONFIG
     public static String getDatabaseURL() {
-        return getValueFromPropertiesKey(PROPERTY_DB_URL);
+        return getValueFromPropertyKey(PROPERTY_DB_URL);
     }
 
     public static String getDatabaseName() {
-        return getValueFromPropertiesKey(PROPERTY_DB_NAME);
+        return getValueFromPropertyKey(PROPERTY_DB_NAME);
     }
 
     public static int getDatabasePort() {
-        return Integer.valueOf(getValueFromPropertiesKey(PROPERTY_DB_PORT));
+        return Integer.valueOf(getValueFromPropertyKey(PROPERTY_DB_PORT));
     }
 
     public static String getDatabaseUser() {
-        return getValueFromPropertiesKey(PROPERTY_DB_USER);
+        return getValueFromPropertyKey(PROPERTY_DB_USER);
     }
 
     public static String getDatabasePassword() {
-        return getValueFromPropertiesKey(PROPERTY_DB_PASSWORD);
+        return getValueFromPropertyKey(PROPERTY_DB_PASSWORD);
+    }
+
+    public static boolean getDatabaseCreationRequest() {
+        boolean value = getValueFromPropertyKey(PROPERTY_DB_CREATION).equals("true");
+
+        if (value) writeValueToPropertyKey(PROPERTY_DB_CREATION, "false");
+
+        return value;
+    }
+
+    public static String getDatabaseCreatioSqlFile() {
+        return getValueFromPropertyKey(PROPERTY_DB_CREATION_FILENAME);
     }
 }
--- src/main/java/com/cyberschnitzel/Util/DatabaseUtil.java
+++ src/main/java/com/cyberschnitzel/Util/DatabaseUtil.java
@@ -1,8 +1,12 @@
 package com.cyberschnitzel.Util;
 
+import com.cyberschnitzel.Domain.Exceptions.ConfigException;
+
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.Semaphore;
 
 public class DatabaseUtil {
@@ -37,6 +41,9 @@ public class DatabaseUtil {
                 // Get the connection
                 connection = DriverManager.getConnection("jdbc:postgresql://" + DATABASE_URL + ":" + PORT + "/" +
                         DATABASE_NAME, USER, PASSWORD);
+
+                // Run the creator script if needed
+                runCreatorScript();
             }
         } catch (InterruptedException ie) {
             System.out.println("Failed to acquire mutex: " + ie.getMessage());
@@ -44,6 +51,8 @@ public class DatabaseUtil {
             System.out.println("SQL Exception thrown: " + sqle.getMessage());
         } catch (ClassNotFoundException e) {
             System.out.println("Failed to find class: " + e.getMessage());
+        } catch (ConfigException ce) {
+            System.out.println("Failed to get config: " + ce.getMessage());
         } finally {
             // Safely release the mutex
             mutex.release();
@@ -83,5 +92,39 @@ public class DatabaseUtil {
      */
     private static boolean checkCredentials() {
         return DATABASE_URL != null && PORT > 0 && DATABASE_NAME != null && USER != null && PASSWORD != null;
+    }
+
+    private static void runCreatorScript() throws SQLException, ConfigException {
+        if (Config.getDatabaseCreationRequest()) {
+            File creatorScriptFile = Config.getResourcesFile(Config.getDatabaseCreatioSqlFile());
+
+            BufferedReader bufferedReader = null;
+            StringBuilder script = new StringBuilder();
+
+            try {
+                // Get file from resources folder
+                bufferedReader = new BufferedReader(new FileReader(creatorScriptFile));
+
+                // Build the script from the file
+                while (bufferedReader.readLine() != null)
+                    script.append(bufferedReader.readLine());
+
+                // Execute the creator script
+                connection.createStatement().execute(script.toString());
+            } catch (FileNotFoundException fnfe) {
+                System.out.println("File not found:" + fnfe.getMessage());
+            } catch (IOException ioe) {
+                System.out.println("I/O Exception:" + ioe.getMessage());
+            } finally {
+                // Safely close the input
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        System.out.println("Failed closing creator script file: " + e.getMessage());
+                    }
+                }
+            }
+        }
     }
 }
