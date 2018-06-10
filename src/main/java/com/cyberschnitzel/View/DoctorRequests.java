7eba921bfafbf73497bb384233b5f36d2e431065
vanpana <van.panaite@gmail.com>
2018-06-10 17:22:52 +0300

    added doctor request window with grid

---
+++ src/main/java/com/cyberschnitzel/View/DoctorRequests.java
@@ -0,0 +1,11 @@
+package com.cyberschnitzel.View;
+
+import com.cyberschnitzel.Domain.Entities.Request;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.VerticalLayout;
+
+public class DoctorRequests extends VerticalLayout {
+    private Grid<Request> requestGrid = new Grid<>();
+
+
+}
